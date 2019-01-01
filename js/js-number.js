8eb22646d6c5804191d2da7ec54fe5a0d655d781
silveirapdu <silveirapdu@gmail.com>
2019-01-01 20:50:03 -0200

    VAr e Identificadores Number

---
+++ js/js-number.js
@@ -0,0 +1,15 @@
+var valor1 = 100;
+var valor2 = 99;
+
+var soma = valor1 + valor2;
+
+console.log(valor1 + valor2);
+
+console.log("O Tipo da varivel valor1 é: " + typeof valor1);
+
+console.log("A soma de valor1 + valor2 em Decimal B10 é: " + soma.toString(10));
+console.log("A soma de valor1 + valor2 em Binario B2 é: " + soma.toString(2));
+console.log("A soma de valor1 + valor2 em Octal B8 é: " + soma.toString(8));
+console.log("A soma de valor1 + valor2 em HexaDecimal B16 é: " + soma.toString(16));
+console.log("Tamanho maximo do number: " + Number.MAX_VALUE);
+console.log("Tamanho minimo do number: " + Number.MIN_VALUE);
