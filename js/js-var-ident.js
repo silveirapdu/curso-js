77569fe4a6723c6d14c0462dc1b1e24c7eb77b29
silveirapdu <silveirapdu@gmail.com>
2019-01-01 20:01:03 -0200

    Variaveis e incicadores com String e TypeOf

---
+++ js/js-var-ident.js
@@ -0,0 +1,18 @@
+var nome = "Cleverson";
+var sobreNome = "Vieira Silveira";
+
+var texto ="texo1 \
+texto2 \
+texto3";
+
+console.log("Nome Completo: " + nome + " " + sobreNome);
+console.log("Texto: " + texto);
+
+var stringVar = "O Tamanho desta Variavel é: ";
+var numberVar1 = 10;
+var numberVar2 = 101.99;
+
+console.log(stringVar + stringVar.length + " Caracteres apartir do indice 0");
+console.log("A variavel stringVar é do tipo: " + typeof stringVar);
+console.log("A variavel numberVar1 é do tipo: " + typeof numberVar1);
+console.log("A variavel numberVar2 é do tipo: " + typeof numberVar2);
