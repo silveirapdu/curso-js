45e2c494b556e876da5077138cda6dd86828eecd
silveirapdu <silveirapdu@gmail.com>
2019-01-01 22:13:33 -0200

    Literais e Construtores

---
+++ js/js.literal.js
@@ -0,0 +1,20 @@
+//Literal e Construtor String
+var curso = "Formação JavaSript - Mestre Jedi";
+var curso2 = new String("A partir do Construtor");
+
+console.log(curso);
+console.log(curso2.toString());
+
+//Literal e contrutor Number
+var num1 = 150;
+var num2 = new Number(136.59);
+
+console.log(num1);
+console.log(num2.toString());
+
+//Literal e contrutor Boolean
+var bool1 = true;
+var bool2 = new Boolean();
+
+console.log(bool1);
+console.log(bool2.toString());
